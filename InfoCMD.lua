792862ed362c3400c88e90d42c2bfd380fd2285f
Dwayne <51810831+Novrb1@users.noreply.github.com>
2020-10-05 05:50:18 -0700

    Made Better NEED SYNAPSE

--- InfoCMD.lua
+++ InfoCMD.lua
@@ -1,4 +1,5 @@
 local lp = game.Players.LocalPlayer
+local plrs = game.Players:GetPlayers()
 lp.Chatted:Connect(function(msg)
 local args = msg:split(" ")
 if args[1]:sub(1,4) == "info" then
@@ -8,6 +9,12 @@ if args[1]:sub(1,4) == "info" then
             msg:FireServer("NAME: " .. v.Name .. " - Age: ss" .. v.AccountAge .. " Days","All")
             local url = "https://www.roblox.com/users/"
             syn_clipboard_set(url .. v.UserId)
+            for _,z in pairs(plrs) do
+            if z:IsFriendsWith(v.UserId) then
+                msg:FireServer(z.Name .. " is friends/playing with " .. v.Name,"All")
+                msg:FireServer("NAME: " .. z.Name .. " - Age: ss" .. z.AccountAge .. " Days","All")
+                end
+                end
             end
         end
     end
